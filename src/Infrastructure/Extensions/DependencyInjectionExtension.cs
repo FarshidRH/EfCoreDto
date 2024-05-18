1a1bfc6bbdab1eaf40132ff995d637568935c62f
Farshid Rezaei <farshidrezaeih@gmail.com>
2024-05-18 10:26:27 +0330

    Add ef configuration.

--- src/Infrastructure/Extensions/DependencyInjectionExtension.cs
+++ src/Infrastructure/Extensions/DependencyInjectionExtension.cs
@@ -1,14 +1,38 @@
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace EfCoreDto.Infrastructure.Extensions;
 
 public static class DependencyInjectionExtension
 {
-	[System.Diagnostics.CodeAnalysis.SuppressMessage("Critical Code Smell", "S1186:Methods should not be empty", Justification = "<Pending>")]
-	[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
-	public static void AddInfrastructureServices(this IServiceCollection service, IConfiguration configuration)
+	public static void AddInfrastructureServices(this IHostApplicationBuilder builder)
 	{
+		builder.Services.AddDbContext<AppDbContext>(opt => opt.ConfigureDbContextOptions(builder));
+	}
 
+	private static DbContextOptionsBuilder ConfigureDbContextOptions(
+		this DbContextOptionsBuilder dbContextOptions, IHostApplicationBuilder host)
+	{
+		var connectionString = host.Configuration.GetConnectionString("SqlServer");
+		dbContextOptions.UseSqlServer(connectionString, sqlServerOptionsBuilder =>
+		{
+			sqlServerOptionsBuilder.EnableRetryOnFailure();
+		});
+
+		if (host.Environment.IsDevelopment())
+		{
+			dbContextOptions.EnableDetailedErrors();
+			dbContextOptions.EnableSensitiveDataLogging();
+			dbContextOptions.ConfigureWarnings(warningsConfigurationBuilder =>
+			{
+				warningsConfigurationBuilder.Log(
+					CoreEventId.FirstWithoutOrderByAndFilterWarning,
+					CoreEventId.RowLimitingOperationWithoutOrderByWarning);
+			});
+		}
+
+		return dbContextOptions;
 	}
 }
